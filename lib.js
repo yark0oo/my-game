27a224409052449e52ce56634c1c83f07f604efc
yark0oo <72916841+yark0oo@users.noreply.github.com>
2021-04-17 11:52:34 +0500

    Add files via upload

--- lib.js
+++ lib.js
@@ -2,31 +2,61 @@ document.onkeydown = checkButton;
 
 function checkButton(event) {
 
+    let cubeRight = cube.x + cube.width;
+    let cubeBottom = cube.y + cube.height;
+
+    let mapRight = map.x + map.width;
+    let mapBottom = map.y + map.height;
+
     if (event.keyCode === 37) {
         console.log("ArrowLeft");
-        if (cube.x > 0) {
-            cube.x -= cube.step;
+        if (map.x < cube.x) {
+            if ((cube.x - map.x) < cube.step) {
+                cube.x -= cube.x - map.x;
+            }
+
+            else {
+                cube.x -= cube.step;
+            }
         }
     }
 
     if (event.keyCode === 38) {
         console.log("ArrowUp");
-        if (cube.y > 0) {
-            cube.y -= cube.step;
+        if (map.y < cube.y) {
+            if ((cube.y - map.y) < cube.step) {
+                cube.y -= cube.y - map.y;
+            }
+
+            else {
+                cube.y -= cube.step;
+            }
         }
     }
 
     if (event.keyCode === 39) {
         console.log("ArrowRight");
-        if ((cube.x + cube.width) < map.width) {
-            cube.x += cube.step;
+        if (cubeRight < mapRight) {
+            if ((mapRight - cubeRight) < cube.step) {
+                cube.x += mapRight - cubeRight;
+            }
+
+            else {
+                cube.x += cube.step;
+            }
         }
     }
 
     if (event.keyCode === 40) {
         console.log("ArrowDown");
-        if ((cube.y + cube.height) < map.height) {
-            cube.y += cube.step;
+        if (cubeBottom < mapBottom) {
+            if ((mapBottom - cubeBottom) < cube.step) {
+                cube.y += mapBottom - cubeBottom;
+            }
+
+            else {
+                cube.y += cube.step;
+            }
         }
     }
 
@@ -48,5 +78,67 @@ function renderMap(map) {
     document.getElementById('map').style.width = map.width + 'px';
     document.getElementById('map').style.height = map.height + 'px';
     document.getElementById('map').style.backgroundColor = map.color;
-    document.getElementById('map').style.border = '4px solid' + map.border;
+    // document.getElementById('map').style.border = '2px solid ' + map.border;
+}
+
+function spawnCube(map, cube) {
+
+    let mapMinX = map.x;
+    let mapMaxX = map.x + map.width - cube.width;
+
+    let mapMinY = map.y;
+    let mapMaxY = map.y + map.height - cube.height;
+
+    cube.x = Math.round(
+        mapMinX + 
+        Math.random() * (mapMaxX - mapMinX)
+        );
+    cube.y = Math.round(
+        mapMinY + 
+        Math.random() * (mapMaxY - mapMinY)
+        );
+
+    return cube;
+}
+
+function renderBots(bots) {
+    bots.map(bot => {
+        let div = document.createElement('div');
+        div.id = 'bot_' + bot.id;
+        div.className = 'bot';
+        document.body.append(div);
+        renderBot(bot);
+    });
+}
+
+function renderBot(bot) {
+    document.getElementById('bot_' + bot.id).style.top = bot.y + 'px';
+    document.getElementById('bot_' + bot.id).style.left = bot.x + 'px';
+    document.getElementById('bot_' + bot.id).style.width = bot.width + 'px';
+    document.getElementById('bot_' + bot.id).style.height = bot.height + 'px';
+    document.getElementById('bot_' + bot.id).style.transition = 'ease ' + bot.trnstn + 's';
+    document.getElementById('bot_' + bot.id).style.backgroundColor = bot.color;
+}
+
+function renderBuffs(buffs) {
+    buffs.map(buff => {
+        let div = document.createElement('div');
+        div.id = 'buff_' + buff.id;
+        div.className = 'buff';
+        document.body.append(div);
+        renderBuff(buff);
+    });
+}
+
+function renderBuff(buff) {
+    document.getElementById('buff_' + buff.id).style.top = buff.y + 'px';
+    document.getElementById('buff_' + buff.id).style.left = buff.x + 'px';
+    document.getElementById('buff_' + buff.id).style.width = buff.width + 'px';
+    document.getElementById('buff_' + buff.id).style.height = buff.height + 'px';
+    document.getElementById('buff_' + buff.id).style.transition = 'ease ' + buff.trnstn + 's';
+    document.getElementById('buff_' + buff.id).style.backgroundColor = buff.color;
+}
+
+function timeTike() {
+    console.log(new Date().getSeconds());
 }
--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 let cube = {
     id: 'cube',
-    x: 0,
-    y: 0,
+    x: 300,
+    y: 300,
     width: 56,
     height: 56,
     trnstn: .25,
     step: 56,
-    color: 'black'
+    color: 'orange'
 }
 
 let map = {
-    x: 0,
-    y: 0,
-    width: 500,
-    height: 500,
+    x: 100,
+    y: 100,
+    width: 470,
+    height: 470,
     color: '#f5f5f5',
     border: '#f1f1f1'
 }
 
-renderCube(cube);
 renderMap(map);
+renderCube(spawnCube(map, cube));
